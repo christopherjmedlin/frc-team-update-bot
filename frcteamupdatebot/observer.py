856bec756c89893b7acc6678c468ae10cd2ae473
Christopher Medlin <cjmedlin99@yahoo.com>
2018-03-25 17:32:57 -0700

    Move methods in FRCTeamUpdateThread that aren't thread specific.

---
+++ frcteamupdatebot/observer.py
@@ -0,0 +1,67 @@
+import requests
+from datetime import date
+
+class FRCTeamUpdateObserver(object):
+
+    TEAM_UPDATE_URL = "https://firstfrc.blob.core.windows.net/frc{}/Manual/TeamUpdates/TeamUpdate{}.pdf"
+
+    def __init__(self, first_team_update_number):
+        self.first_team_update_number = first_team_update_number
+        self._find_last_team_update()
+
+    def _get_url(self, next_update=False):
+        team_update = self.last_team_update
+        if next_update:
+            team_update[1] += 1
+        team_update_number = str(team_update[1])
+        if team_update[1] < 10:
+            # add trailing zero for numbers less than 10
+            team_update_number = '0' + team_update_number
+
+        return self.TEAM_UPDATE_URL.format(
+            team_update[0], team_update_number
+        )
+
+    def _find_last_team_update(self):
+        """
+        Increments the second element in last_team_update until the last posted
+        team update has been found with requests.
+        """
+        #TODO change back from 18 to 10 before production
+        self.last_team_update = [date.today().year, 8]
+        loop = True
+        while loop:
+            self.last_team_update[1] += 1
+            r = requests.get(self._get_url())
+            print(self._get_url())
+            if r.status_code != 200:
+                self.last_team_update[1] -= 1
+                loop = False
+
+    def _check_year(self):
+        """
+        Checks if there is a new year. If there is, the team update number is
+        reset to 10 and the year is updated.
+        """
+        if (self.last_team_update[0] < date.today().year):
+            self.last_team_update[0] = date.today().year
+            self.last_team_update[1] = self.first_team_update_number
+
+    def check_for_team_updates(self):
+        """
+        Checks for a new FRC Team Update based on last_team_update values
+        
+        If it is found, the next time the URL is checked it will look for the next
+        team update.
+
+        :returns: None if the team update wasn't found, the url otherwise
+        """
+        self._check_year()
+        url = self._get_url(next_update=True)
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            self.last_team_update[1] += 1
+            return url
+        else:
+            return None
--- frcteamupdatebot/thread.py
+++ frcteamupdatebot/thread.py
@@ -7,6 +7,7 @@ import time
 import urllib
 from datetime import date
 import asyncio
+from .observer import FRCTeamUpdateObserver
 
 class FRCTeamUpdateThread(threading.Thread):
     """
@@ -17,7 +18,9 @@ class FRCTeamUpdateThread(threading.Thread):
     Discord client object from two seperate processes.
     """
 
+    #TODO change to 10 for production
     FIRST_TEAM_UPDATE_NUMBER = 18
+
     TEAM_UPDATE_URL = "https://firstfrc.blob.core.windows.net/frc{}/Manual/TeamUpdates/TeamUpdate{}.pdf"
     TIMESTAMP_FORMAT = "'%Y-%m-%d'"
     TEAM_UPDATE_MESSAGE = ("New FRC Team Update:\n"
@@ -25,67 +28,21 @@ class FRCTeamUpdateThread(threading.Thread):
 
     def __init__(self, event_loop):
         threading.Thread.__init__(self)
-        self.last_team_update = [date.today().year, self.FIRST_TEAM_UPDATE_NUMBER]
+        self.observer = FRCTeamUpdateObserver(self.FIRST_TEAM_UPDATE_NUMBER)
         self.event_loop = event_loop
-        self._find_last_team_update()
 
     def run(self):
         # overrides threading.Thread.run()
         while True:
-            self._check_year()
-            pdf_url = self._check_for_team_updates()
+            pdf_url = self.observer.check_for_team_updates()
             if pdf_url:
                 pdf_url = save_to_web_archive(pdf_url)
                 message = self.TEAM_UPDATE_MESSAGE.format(pdf_url)
                 send_message_to_channels_in_db(
                     message, db, self.event_loop
                 )
-                self.last_team_update[1] += 1
             time.sleep(1800.0)
 
-    def _find_last_team_update(self):
-        """
-        Increments the second element in last_team_update until the last posted
-        team update has been found with requests.
-        """
-        #TODO change back from 18 to 10 before production
-        self.last_team_update = [date.today().year, 18]
-        loop = True
-        while loop:
-            self.last_team_update[1] += 1
-            r = requests.get(
-                self.TEAM_UPDATE_URL.format(
-                    self.last_team_update[0], self.last_team_update[1]
-                )
-            )
-            if r.status_code != 200:
-                self.last_team_update[1] -= 1
-                loop = False
-
-    def _check_year(self):
-        """
-        Checks if there is a new year. If there is, the team update number is
-        reset to 10 and the year is updated.
-        """
-        if (self.last_team_update[0] < date.today().year):
-            self.last_team_update[0] = date.today().year
-            self.last_team_update[1] = self.FIRST_TEAM_UPDATE_NUMBER
-
-    def _check_for_team_updates(self):
-        """
-        Checks for a new FRC Team Update based on last_team_update values
-
-        :returns: None if the team update wasn't found, the url otherwise
-        """
-        url = FRCTeamUpdateThread.TEAM_UPDATE_URL.format(
-            self.last_team_update[0], self.last_team_update[1] + 1
-        )
-        r = requests.get(url)
-
-        if r.status_code == 200:
-            return url
-        else:
-            return None
         
 WEB_ARCHIVE_HOST = "https://web.archive.org"
 WEB_ARCHIVE_SAVE_URL = "https://web.archive.org/save/"

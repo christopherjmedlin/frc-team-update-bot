02c8259ef7bd9b59fd0915a0294fdfb42be4c798
Christopher Medlin <cjmedlin99@yahoo.com>
2018-04-09 20:28:00 -0700

    Got rid of multithreading, use call_later for the interval instead

--- frcteamupdatebot/client.py
+++ frcteamupdatebot/client.py
@@ -6,6 +6,7 @@ import logging
 import sys
 import requests
 from psycopg2 import errorcodes
+from .observer import FRCTeamUpdateObserver
 
 HELP_MESSAGE = """Type '!frctu mark' to mark a channel for team updates.
 Type '!frctu unmark' to unmark a channel for team updates."""
@@ -13,13 +14,16 @@ Type '!frctu unmark' to unmark a channel for team updates."""
 ADMIN_IDS = ['188472809280897024', '117126246168657924']
 DEFAULT_PSYCOPG2_SETTINGS = "dbname=postgres user=postgres password=postgres host=172.19.0.3"
 PSYCOPG2_SETTINGS = os.getenv("PSYCOPG2_SETTINGS", default=DEFAULT_PSYCOPG2_SETTINGS)
+TEAM_UPDATE_URL = "https://firstfrc.blob.core.windows.net/frc{}/Manual/TeamUpdates/TeamUpdate{}.pdf"
+TIMESTAMP_FORMAT = "'%Y-%m-%d'"
+TEAM_UPDATE_MESSAGE = ("New FRC Team Update:\n"
+                        "{}")   
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 client = discord.Client()
 
 # import after stuff is defined
-from .thread import FRCTeamUpdateThread
 
 @client.event
 async def on_ready():
@@ -31,9 +35,11 @@ async def on_ready():
     if os.getenv("DISABLE_TEAM_UPDATE_OBSERVER", default=False) == 'True':
         pass
     else:
-        print("Starting thread...")
-        FRCTeamUpdateThread(asyncio.get_event_loop()).start()
-        print("Thread started.")
+        if "FRCTU_LAST_TEAM_UPDATE" in os.environ:
+            observer = FRCTeamUpdateObserver(last_team_update=int(os.environ["FRCTU_LAST_TEAM_UPDATE"]))
+        else:
+            observer = FRCTeamUpdateObserver()
+        observe(observer)
 
 #TODO holy shit i need to refactor this mess
 @client.event
@@ -92,3 +98,49 @@ async def on_message(message):
                         )
             else:
                 await client.send_message(message.channel, "Administrator priveleges are required for command '" + command[1] + "'.")
+
+def observe(observer):
+    client.loop.call_later(1800, observe, observer)
+    pdf_url = observer.check_for_team_updates()
+    if pdf_url:
+        pdf_url = save_to_web_archive(pdf_url)
+        message = TEAM_UPDATE_MESSAGE.format(pdf_url)
+        send_message_to_channels_in_db(
+            message, psycopg2.connect(PSYCOPG2_SETTINGS), client.loop
+        )
+
+WEB_ARCHIVE_HOST = "https://web.archive.org"
+WEB_ARCHIVE_SAVE_URL = "https://web.archive.org/save/"
+
+def save_to_web_archive(url):
+    """
+    Saves a webpage to WebArchive using the Wayback Machine
+
+    :param url: the url of the webpage
+    
+    :returns: the resulting WebArchive url
+    """
+    return WEB_ARCHIVE_HOST + requests.get(
+        WEB_ARCHIVE_SAVE_URL + url,
+        allow_redirects=False
+    ).headers['Content-Location']
+
+def send_message_to_channels_in_db(message, db, event_loop):
+    """
+    Sends the message to all of the channels currently contained in the
+    PostgreSQL database
+
+    :param message: the message
+    :param db: the psycopg2 connection
+    :param event_loop: the event loop to use when sending the messages
+    """
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM channels;")
+    for record in cursor:
+        channel = client.get_channel(str(record[0]))
+        #TODO delete the channel from DB if it doesn't exist
+        if channel:
+            # event_loop.create_task(client.send_file(channel, file))
+            event_loop.create_task(client.send_message(channel, message))
+    cursor.close()
+    db.close()
--- frcteamupdatebot/thread.py
+++ frcteamupdatebot/thread.py
@@ -19,10 +19,7 @@ class FRCTeamUpdateThread(threading.Thread):
     Discord client object from two seperate processes.
     """
 
-    TEAM_UPDATE_URL = "https://firstfrc.blob.core.windows.net/frc{}/Manual/TeamUpdates/TeamUpdate{}.pdf"
-    TIMESTAMP_FORMAT = "'%Y-%m-%d'"
-    TEAM_UPDATE_MESSAGE = ("New FRC Team Update:\n"
-                           "{}")                 
+                  
 
     def __init__(self, event_loop):
         threading.Thread.__init__(self)
@@ -43,40 +40,3 @@ class FRCTeamUpdateThread(threading.Thread):
                     message, psycopg2.connect(PSYCOPG2_SETTINGS), self.event_loop
                 )
             time.sleep(1800.0)
-
-        
-WEB_ARCHIVE_HOST = "https://web.archive.org"
-WEB_ARCHIVE_SAVE_URL = "https://web.archive.org/save/"
-
-def save_to_web_archive(url):
-    """
-    Saves a webpage to WebArchive using the Wayback Machine
-
-    :param url: the url of the webpage
-    
-    :returns: the resulting WebArchive url
-    """
-    return WEB_ARCHIVE_HOST + requests.get(
-        WEB_ARCHIVE_SAVE_URL + url,
-        allow_redirects=False
-    ).headers['Content-Location']
-
-def send_message_to_channels_in_db(message, db, event_loop):
-    """
-    Sends the message to all of the channels currently contained in the
-    PostgreSQL database
-
-    :param message: the message
-    :param db: the psycopg2 connection
-    :param event_loop: the event loop to use when sending the messages
-    """
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM channels;")
-    for record in cursor:
-        channel = client.get_channel(str(record[0]))
-        #TODO delete the channel from DB if it doesn't exist
-        if channel:
-            # event_loop.create_task(client.send_file(channel, file))
-            event_loop.create_task(client.send_message(channel, message))
-    cursor.close()
-    db.close()
